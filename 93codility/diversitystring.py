372360921fa70c4afafcfcbdd051c443b92bd024
josephbill <josephbill00@gmail.com>
2023-11-02 12:55:54 +0300

    diversity string questions

---
+++ 93codility/diversitystring.py
@@ -0,0 +1,28 @@
+def solution(N):
+    # get from 26 the occurances of uniqueness that we can get 
+    # reptition of the distincts letters 
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if N <= 26:
+        return ''.join(chr(97 + i) for i in range(N))
+    
+    # maximum number of distinct letters 
+    max_letters = 1
+    for i in range(1,27): # 26 letters 
+        if N % i == 0:
+            max_letters = i 
+    print(max_letters)
+    # repetition 
+    repetition = N // max_letters
+    print(repetition)
+
+    # result 
+    result = ''
+    for i in range(max_letters):
+        result += chr(97 + i) * repetition
+
+    return result 
+
+
+
+print(solution(31))
--- python/loops/loops.py
+++ python/loops/loops.py
@@ -1,20 +1,47 @@
-def print_asters(n):
-    for i in range(1, n + 1):
-        row = " "
-        for j in range(1, i + 1):
-             row += "* "
+# pseudocode bit of this : 
+# <!-- What's are the knows -->
+# 1. n is a positive integer (absolute)
+# 2. printing starts at 1 
+# 3. printing ends at 4
+# 4. value increase by 1 on each iteration 
+# 5. space between asterisks (format for the output)
+# 6. n represents the rows 
+# range(start,stop,step)
+
+def solution(n):
+    for i in range(1,n+1):
+        row = ''
+        for j in range(1,i + 1):
+            row += "* "
         print(row)
-    
 
-print(print_asters(4))
-
-
-# upside down print 
+print(solution(4))
+        
 def upside_down(n):
-    for i in range(n, 0, -1):  # Reverse the loop order
-        row = ' '.join(['*' for _ in range(i)])  # Create the row with asterisks
+    for i in range(n,0,-1):
+        row = ' '.join(["*" for j in range(i)])
         print(row)
-
-print_asters(4)
 
 print(upside_down(4))
+
+
+arrayofnumbers = [1,2,3,4,5]
+
+for num in range(len(arrayofnumbers)):
+    print(arrayofnumbers[num])
+
+
+n = 4 
+for i in range(1,n+1):
+    print(i)
+
+
+def negative(temperatures):
+    days = 0
+    for t in temperatures:
+        print(t)
+        if t < 0:
+            days += 1
+    return days
+
+print(negative([6, 7, 8, 8]))  # Output: 0
